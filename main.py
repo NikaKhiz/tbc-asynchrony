812240d3c5c9cffec4e0f31fb626000440d82443
NikaKhiz <khizambarelinika123@gmail.com>
2024-09-15 04:35:46 +0400

    refactor code define client session outside get posts function and pass session as argument

--- main.py
+++ main.py
@@ -10,46 +10,46 @@ num_of_posts_written = 0
 async def main():
     start_time = time.perf_counter()
 
-    await fetch_and_write_posts()
-    
+    async with aiohttp.ClientSession() as session:
+        await fetch_and_write_posts(session)
+
     end_time = time.perf_counter()
     print(f"Time taken: {end_time - start_time:.2f} seconds")
 
 
 # fetch post from url according to the given post id
-async def get_post(post_id):
+async def get_post(session, post_id):
     global num_of_posts_written
-    session = aiohttp.ClientSession()
+    
+    try:
+        response = await session.get(url.format(post_id))
+        post = await response.json()
 
-    async with aiohttp.ClientSession() as session:
-        try:
-            response = await session.get(url.format(post_id))
-            post = await response.json()
-            with lock:
-                with open('posts.json', 'a') as file:
-                    json.dump(post, file, indent=4)
+        with lock:
+            with open('posts.json', 'a') as file:
+                json.dump(post, file, indent=4)
 
-                    # increase number of posts written
-                    # to append necessary characters in to the file
-                    num_of_posts_written += 1
+                # increase number of posts written
+                # to append necessary characters in to the file
+                num_of_posts_written += 1
 
-                    if num_of_posts_written < num_of_posts:
-                        file.write(',\n')
-                    else:
-                        file.write('\n]')
+                if num_of_posts_written < num_of_posts:
+                    file.write(',\n')
+                else:
+                    file.write('\n]')
 
-        except aiohttp.ClientConnectorError as e:
-            print(f"Error fetching post {post_id}: {e}")
+    except aiohttp.ClientConnectorError as e:
+        print(f"Error fetching post {post_id}: {e}")
 
 # truncate file and set its initial state
 # fetch posts simultaneously and write them in to the file
-async def fetch_and_write_posts():
+async def fetch_and_write_posts(session):
     with open('posts.json', 'a') as file:
         file.seek(0)
         file.truncate()
         file.write('[\n')
 
-    await asyncio.gather(*(get_post(id) for id in range(1, num_of_posts + 1)))
+    await asyncio.gather(*(get_post(session, id) for id in range(1, num_of_posts + 1)))
 
 
 if __name__ == "__main__":
